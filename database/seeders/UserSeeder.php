05849bb5dd6a91362e1b6a3dbc76082f836081d3
Hiikey7 <kelvinramsiel@gmail.com>
2025-10-21 23:54:50 +0300

    feat: Update user seeder with collaboration features
    - Add collaboration-related user attributes
    - Update seeder logic for new requirements

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -34,5 +34,27 @@ class UserSeeder extends Seeder
             'updated_at' => '2025-10-08 14:26:00',
             'deleted_at' => null,
         ])->assignRole('developer');
+        \App\Models\User::create([
+            'id' => 2,
+            'username' => 'Reviewer001',
+            'first_name' => 'Reviewer',
+            'other_names' => '001',
+            'gender' => 'male',
+            'mobile_phone' => '(+254) 712-345678',
+            'email' => 'reviewer001@example.com',
+            'email_verified_at' => null,
+            'google_id' => '111693591448676730118',
+            'account_status' => 'active',
+            'terms_accepted' => true,
+            'terms_accepted_count' => 1,
+            'last_terms_accepted_at' => '2025-10-08 14:26:00',
+            'current_terms_version' => '1.0',
+            'points' => 50,
+            'password' => '$2y$12$ULS46OY1s2IP0fiLoJ3/D.uEONgujthJdvSa1.Jn9RQ8MfKAvq.8u',
+            'remember_token' => 'F6IySiAKzEMQFKPFYtXuBPjeLBmOLgAyEPIIv3n3kQvnUs2QRTjjHMhYMBHd',
+            'created_at' => '2025-10-08 14:24:32',
+            'updated_at' => '2025-10-08 14:26:00',
+            'deleted_at' => null,
+        ])->assignRole('idea_reviewer');
     }
 }
