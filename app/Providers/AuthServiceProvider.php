175af7e18f56e29878612d2bfe4f33bf3834bb8f
Hiikey7 <kelvinramsiel@gmail.com>
2025-10-21 23:55:17 +0300

    feat: Register policies in AuthServiceProvider
    - Register IdeaPolicy and other policies
    - Configure authorization services

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Idea;
+use App\Policies\IdeaPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Idea::class => IdeaPolicy::class,
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
